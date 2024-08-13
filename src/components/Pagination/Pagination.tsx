0aa8c31215cc01e0e2e1af00067e126c6c44189d
DevRish <rishavchatterjee1546@gmail.com>
2024-08-13 13:48:10 +0530

    fixed pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,19 +8,19 @@ interface IPaginationProps {
 }
 
 const Pagination = ({ page, limit, setPage, total } : IPaginationProps) => {
-    console.log((total/limit) + ((total%limit === 0) ? 0 : 1))
+    console.log(Math.floor(total/limit) + ((total%limit === 0) ? 0 : 1))
     return (
         <div className="pagination">
             <div className="control" onClick={() => {
                 if(page > 1) setPage(page-1);
             }}><i className="fa-solid fa-chevron-left"></i></div>
             {
-                [...Array(Math.round(total/limit) + ((total%limit === 0) ? 0 : 1)).keys()].map((e) => {
+                [...Array(Math.floor(total/limit) + ((total%limit === 0) ? 0 : 1)).keys()].map((e) => {
                     return <div className={(page === (e+1)) ? "active" : ""} onClick={() => { setPage(e+1) }}>{e+1}</div>
                 })
             }
             <div className="control" onClick={() => {
-                if(page < (Math.round(total/limit) + ((total%limit === 0) ? 0 : 1))) setPage(page+1);
+                if(page < (Math.floor(total/limit) + ((total%limit === 0) ? 0 : 1))) setPage(page+1);
             }}><i className="fa-solid fa-chevron-right"></i></div>
         </div>
     );
